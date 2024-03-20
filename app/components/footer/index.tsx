538e4543955ce29d72889aa512d4e81af2c772df
Hélène BAUDRAND <Helene@MacBook-Pro-de-apixis.local>
2024-03-20 17:07:22 +0100

    style(footer): sponsors logos

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -1,7 +1,41 @@
+import Image from "next/image";
+
 export default function Footer() {
   return (
     <main className="w-full h-20 absolute bottom-0 bg-sky-900 text-white">
-      Footer
+      <div className="flex justify-center gap-10 items-center h-full">
+        <Image
+          src="/images/logo_monkeypatch.jpg"
+          alt="Logo MonkeyPatch"
+          width="140"
+          height="20"
+          className="relative"
+        />
+
+        <Image
+          src="/images/logo_harrycow.jpg"
+          alt="Logo Harrycow"
+          width="60"
+          height="20"
+          className="relative"
+        />
+
+        <Image
+          src="/images/logo_nextjs.jpg"
+          alt="Logo Next.js"
+          width="80"
+          height="20"
+          className="relative"
+        />
+
+        <Image
+          src="/images/logo_qwik.jpg"
+          alt="Logo Qwik"
+          width="140"
+          height="20"
+          className="relative"
+        />
+      </div>
     </main>
   );
 }
